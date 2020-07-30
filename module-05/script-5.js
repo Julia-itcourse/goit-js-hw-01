9584cf34825a359e6d62936ab8d4fc1422153586
Julia-itcourse <66180630+Julia-itcourse@users.noreply.github.com>
2020-07-30 08:25:45 +0200

    добавляет дз-5

---
+++ module-05/script-5.js
@@ -0,0 +1,266 @@
+'use strict';
+
+//? TASK 1
+//* line 16 how to fix? why in login retuns obj and in email undefined?
+
+// Напиши функцию - конструктор Account, которая создает
+// объект со свойствами login и email.В prototype
+// функции - конструктора добавь метод getInfo(),
+// который выводит в консоль значения полей
+// login и email объекта который его вызвал.
+
+// const Account = function (login, email) {
+//   this.login = login;
+//   this.email = email;
+// };
+
+// Account.prototype.getInfo = function () {
+//   console.log(`Login: ${this.login}, Email: ${this.email}`);
+// };
+
+// console.log(Account.prototype.getInfo); // function
+
+// const mango = new Account({
+//   login: 'Mangozedog',
+//   email: 'mango@dog.woof',
+// });
+
+// mango.getInfo(); // Login: Mangozedog, Email: mango@dog.woof
+
+// const poly = new Account({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// poly.getInfo(); // Login: Poly, Email: poly@mail.com
+
+//? TASK 2
+//* To fix the same issue with object as in task 1
+// Напиши класс User для создания пользователя со следующим
+// свойствами:
+
+// name - строка
+// age - число
+// followers - число
+
+// Добавь метод getInfo(),
+//     который, выводит строку:
+// User ${ имя } is ${ возраст } years old and
+// has ${ кол - во фоловеров } followers
+
+// class User {
+//   constructor(name, age, followers) {
+//     this.name = name;
+//     this.age = age;
+//     this.followers = followers;
+//   }
+//   getInfo() {
+//     console.log(`User ${this.name} is ${this.age} years old and
+// has ${this.followers} followers`);
+//   }
+// }
+
+// const mango = new User({
+//   name: 'Mango',
+//   age: 2,
+//   followers: 20,
+// });
+
+// mango.getInfo(); // User Mango is 2 years old and has 20 followers
+
+// const poly = new User({
+//   name: 'Poly',
+//   age: 3,
+//   followers: 17,
+// });
+
+// poly.getInfo(); // User Poly is 3 years old and has 17 followers
+
+//? TASK 3
+
+// class Storage {
+//   constructor(items) {
+//     this.items = items;
+//   }
+
+//   getItems() {
+//     return this.items;
+//   }
+//   addItem(item) {
+//     items.push(item);
+//   }
+
+//   removeItem(item) {
+//     for (let i = 0; i < items.length; i++) {
+//       if (items[i] === item) {
+//         items.splice(i, 1);
+//       }
+//     }
+//   }
+// }
+
+// const storage = new Storage([
+//   'Нанитоиды',
+//   'Пролонгер',
+//   'Железные жупи',
+//   'Антигравитатор',
+// ]);
+
+// const items = storage.getItems();
+// console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+
+// storage.addItem('Дроид');
+// console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+// storage.removeItem('Пролонгер');
+// console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
+
+//? TASK 4
+
+//* append(str) {
+//*this._value = this._value.concat(str);
+//*  } why works with this._value and throws an error with this.value
+//* as there  is only getter?
+
+// class StringBuilder {
+//   constructor(str) {
+//     this._value = str;
+//   }
+//   get value() {
+//     return this._value;
+//   }
+
+//   append(str) {
+//     this._value = this._value.concat(str);
+//   }
+//   prepend(str) {
+//     this._value = str.concat(this._value);
+//   }
+
+//   pad(str) {
+//     this._value = str.concat(this._value, str);
+//   }
+// }
+
+// const builder = new StringBuilder('.');
+
+// builder.append('^');
+// console.log(builder.value); // '.^'
+
+// builder.prepend('^');
+// console.log(builder.value); // '^.^'
+
+// builder.pad('=');
+// console.log(builder.value); // '=^.^='
+
+//? TASK 5
+
+class Car {
+  /*
+   * Добавь статический метод `getSpecs(car)`,
+   * который принимает объект-машину как параметр и выводит
+   * в консоль значения свойств maxSpeed, speed, isOn, distance и price.
+   */
+
+  /*
+   * Конструктор получает объект настроек.
+   *
+   * Добавь свойства будущеего экземпляра класса:
+   *  speed - текущая скорость, изначально 0
+   *  price - цена автомобиля
+   *  maxSpeed - максимальная скорость
+   *  isOn - заведен ли автомобиль, значения true или false. Изначально false
+   *  distance - общий киллометраж, изначально 0
+   */
+  constructor(speed = 0, price, maxSpeed, isOn = false, distance = 0) {
+    this._speed = speed;
+    this._price = price;
+    this._maxSpeed = maxSpeed;
+    this._isOn = isOn;
+    this._distance = distance;
+  }
+
+  get price() {
+    return this._price;
+  }
+  set price(price) {
+    return (this._price = carPrice); //* Clarify names for setter and parameter it takes
+  }
+  static getSpecs(car) {
+    console.log(`Максимальная скорость: ${this.maxSpeed}, текущая скорость: ${this.speed}, заведен ли автомобиль: ${this.isOn}, 
+  общий киллометраж: ${this.distance}, цена: ${this.price}`);
+  }
+  /*
+   * Добавь геттер и сеттер для свойства price,
+   * который будет работать с свойством цены автомобиля.
+   */
+
+  /*
+   * Добавь код для того чтобы завести автомобиль
+   * Записывает в свойство isOn значение true
+   */
+  turnOn() {
+    this._isOn = true;
+  }
+
+  /*
+   * Добавь код для того чтобы заглушить автомобиль
+   * Записывает в свойство isOn значение false,
+   * и сбрасывает текущую скорость в 0
+   */
+  turnOff() {
+    this._isOn = false;
+    this._speed = 0;
+  }
+
+  /*
+   * Добавялет к свойству speed полученное значение,
+   * при условии что результирующая скорость
+   * не больше чем значение свойства maxSpeed
+   */
+  accelerate(value) {
+    if (this._speed + value < this._maxSpeed) {
+      this._speed += value;
+    }
+  }
+
+  /*
+   * Отнимает от свойства speed полученное значение,
+   * при условии что результирующая скорость не меньше нуля
+   */
+  decelerate(value) {
+    if (this._speed - value >= 0) {
+      this._speed -= value;
+    }
+  }
+
+  /*
+   * Добавляет в поле distance киллометраж (hours * speed),
+   * но только в том случае если машина заведена!
+   */
+  drive(hours) {
+    if (this._isOn === true) {
+      this._distance = hours * this._speed;
+    }
+  }
+}
+
+const mustang = new Car({ maxSpeed: 200, price: 2000 });
+
+mustang.turnOn();
+mustang.accelerate(50);
+mustang.drive(2);
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 50, isOn: true, distance: 100, price: 2000
+
+mustang.decelerate(20);
+mustang.drive(1);
+mustang.turnOff();
+
+Car.getSpecs(mustang);
+// maxSpeed: 200, speed: 0, isOn: false, distance: 130, price: 2000
+
+console.log(mustang.price); // 2000
+mustang.price = 4000;
+console.log(mustang.price); // 4000
